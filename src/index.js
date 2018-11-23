e4fb9cf3459914c0a50b817104eb0009cf9a86a8
mdmirajulkarim <mdmirajulkarim@gmail.com>
2018-11-23 22:39:02 +0600

    calculator

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./component/App";
@@ -5,3 +6,13 @@ import "./index.css";
 import "github-fork-ribbon-css/gh-fork-ribbon.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+=======
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Calculator from './Calculator';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<Calculator />, document.getElementById('root'));
+registerServiceWorker();
+>>>>>>> ready
